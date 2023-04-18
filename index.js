4e08dca868266e5e8a1f91118eb0cb6c2c469e4f
felipe <loyola>
2023-04-17 18:17:12 -0700

    rendering lobby

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {Lobby} from './src/Screens';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Lobby);
