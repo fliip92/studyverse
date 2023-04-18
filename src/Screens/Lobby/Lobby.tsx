3988cb7f18c13313dbfead1c52a923f244dcf34b
felipe <loyola>
2023-04-17 21:07:01 -0700

    updating lobby

--- src/Screens/Lobby/Lobby.tsx
+++ src/Screens/Lobby/Lobby.tsx
@@ -1,10 +1,29 @@
-import React, {FC} from 'react';
-import {View} from 'react-native';
+import React, {FC, useRef} from 'react';
+import {Button, View} from 'react-native';
+import {TwilioVideoCallModule} from '../../modules';
+import Config from 'react-native-config';
 import {styles} from './styles';
 import {MainScreenProps} from './types';
 
+const {VideoView} = TwilioVideoCallModule;
+
 const Lobby: FC<MainScreenProps> = () => {
-  return <View style={styles.container} />;
+  const videoViewRef = useRef();
+
+  const onJoinRoom = async () => {
+    TwilioVideoCallModule.setVideoView(videoViewRef.current);
+    await TwilioVideoCallModule.joinRoom(
+      'felipesRoom',
+      Config.ACCESS_TOKEN as string,
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <VideoView ref={videoViewRef} style={{width: '100%', height: '100%'}} />
+      <Button title="Join Room" onPress={onJoinRoom} />
+    </View>
+  );
 };
 
 export default Lobby;
